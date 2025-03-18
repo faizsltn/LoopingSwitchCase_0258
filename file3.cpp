acf35ba0a3f7abb8c1fdad2053da6c2b06612302
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-03-18 11:57:21 +0700

    membuat fungsi

--- file3.cpp
+++ file3.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+float hitungLuasPersegiPanjang(float p, float l){
+    return p * l;
+}
+
+float hitungLuasSegitiga(float a, float t){
+    return 0.5 * a * t;
+}
