4f30a24a41a94c1cc5350146c3ae98d129060256
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-03-18 11:39:36 +0700

    menambahkan perulangan while dan do while

--- file2.cpp
+++ file2.cpp
@@ -2,3 +2,19 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+int main(){
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile =0;
+    // Perulangan while
+    cout << "perulangan dengan while" << endl;
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Nilai awal x = " << x << endl;
+    while(x < 7){
+        cout << "nilai x = " << x << " Lari keliling lapangan" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+}
