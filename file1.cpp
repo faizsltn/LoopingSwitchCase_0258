61ef9be0312b3e56489874904ef0e383ad273d82
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-03-18 11:02:12 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cacah;
+    //perulangan for untuk pencacah naik
+    for(cacah = 0; cacah <= 4; cacah++){
+        cout << "Nilai Pencacah = " << cacah << " Selamat Datang!" << endl;
+    }
+    cout << "Nilai pencacah terakhir = " << cacah;
+    cout << endl;
+    cout << endl;
+
+    // perulangan for untuk pencacah turun
+    for(cacah = 5; cacah > 0; cacah--){
+        cout << "Nilai Pencacah = " << cacah << " Selamat Datang!" << endl;
+    }
+    
+}
