903bfc2a8987b8c9d9cea34c4fb1b537b8ab08e2
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-03-18 11:48:24 +0700

    menambahkan perulangan dowhile

--- file2.cpp
+++ file2.cpp
@@ -17,4 +17,15 @@ int main(){
         x = rand() % 10;
         perulanganWhile++;
     }
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << endl;
+    cout << "Perulangan dengan do while" << endl;
+    do{
+        cout << "nilai x = " << x << " Lari keliling lapangan!" << endl;
+        x = rand() % 10;
+        perulanganDoWhile++;
+    }while(x < 7);
+    cout << "Nilai akhir x = " << x << endl;
+    cout << "Jumlah perulangan = " << perulanganDoWhile << endl;
 }
